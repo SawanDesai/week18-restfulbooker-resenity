e7333e2606d53024628a588f4f03b60cbef99ed0
Pankhida <DDesai0751@gmail.com>
2023-01-08 11:39:36 +0000

    myhomeworkweek18

---
+++ src/main/java/com/restful/booker/petstoreinfo/constants/Path.java
@@ -0,0 +1,14 @@
+package com.restful.booker.petstoreinfo.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for student api
+     */
+    public static final String AUTHORIZATION = "/auth";
+    public static final String BOOKING = "/booking";
+    public static final String PING = "/ping";
+
+}
